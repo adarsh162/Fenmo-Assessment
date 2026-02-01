5fa8292d74fd013d2091abf05ebce4f37b7a1e08
adarsh162 <adarsh161202@gmail.com>
2026-02-01 13:47:13 +0530

    backend: fixed typo in router.py

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,6 +1,6 @@
-from app.api.v1 import items, expenses
+from app.api.v1 import items, expense
 from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(items.router, prefix="", tags=["items"])
-router.include_router(expenses.router, prefix="/expenses", tags=["expenses"])
+router.include_router(expense.router, prefix="/expenses", tags=["expenses"])
