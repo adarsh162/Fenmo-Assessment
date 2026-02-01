de8bba97bbdeba10d759024170bc5dc9bb689413
adarsh162 <adarsh161202@gmail.com>
2026-02-01 14:18:23 +0530

    frontend: Added expenseService API

---
+++ frontend/src/api/expense.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+export const expenseService = {
+  getExpenses: (category = '', sort = 'date_desc') => {
+    const params = new URLSearchParams();
+    if (category) params.append('category', category);
+    if (sort) params.append('sort', sort);
+    return api.get(`/expenses/?${params.toString()}`);
+  },
+
+  createExpense: (expenseData) => {
+    return api.post('/expenses/', expenseData);
+  }
+};
